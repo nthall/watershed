544f373cbff2ac5026fcbff3a012756a3f256320
Noah Hall <noah.t.hall@gmail.com>
2016-12-15 01:00:11 +0000

    add handy methods to custom ItemManager, fix initial position calc, and add helpers for moving individual items within the queue

--- api/models.py
+++ api/models.py
@@ -17,6 +17,14 @@ PLATFORMS = (
 )
 
 
+class ItemManager(models.Manager):
+    def queue(user):
+        return Item.objects.filter(user=user, position__gte=0)
+
+    def history(user):
+        return Item.objects.filter(user=user, position__lt=0)
+
+
 class Item(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='items',
                              on_delete=models.CASCADE)
@@ -32,6 +40,8 @@ class Item(models.Model):
     fave = models.BooleanField(default=False)
     # tags = fields.ArrayField (postgres-specific, so, todo i guess?)
 
+    objects = ItemManager()
+
     class Meta:
         ordering = ('position',)
 
@@ -39,7 +49,7 @@ class Item(models.Model):
         '''
         todo: allow checkbox option to save at front of list?
         '''
-        queue = Item.objects.filter(user=self.user)
+        queue = Item.objects.queue(self.user)
         self.position = len(queue)
 
         if (self.platform == 3):
@@ -56,8 +66,33 @@ class Item(models.Model):
 
         super(Item, self).save(*args, **kwargs)
 
+    def move(self, new_position):
+        ''' move a track and adjust the rest of the queue around it '''
+        old_position = self.position
+        self.position = new_position
+        queue = Item.objects.queue()
+        if old_position > new_position:
+            adjust = 1
+            items = queue.exclude(position__gte=old_position, position__lte=0)
+        else:
+            adjust = -1
+            items = queue.filter(position__gte=old_position)
+        for item in items:
+            item.position += adjust
+            item.save()
+        self.save()
+
+    def move_to_front(self):
+        ''' move an item to position 1 '''
+        self.move(1)
+
+    def move_to_back(self):
+        ''' move an item to the end of the queue '''
+        self.move(len(Item.objects.queue()))
+
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
+    ''' create auth token on user create '''
     if created:
         Token.objects.create(user=instance)

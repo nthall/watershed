c4a2555ddd1770a2e76ce3bed948793381403494
Noah Hall <noah.t.hall@gmail.com>
2017-01-06 16:59:25 -0500

    add bandcamp autoplay and start work on checking bandcamp start track

--- browser/chrome/js/bandcamp.js
+++ browser/chrome/js/bandcamp.js
@@ -1,6 +1,17 @@
 play = document.getElementById("big_play_button");
-if (play) {
 
+if (play) {  // only run logic in bandcamp iframe
+  tracklist = document.getElementById("tracklist_ul");
+
+  if (tracklist) {
+    loaded_track = document.getElementById("currenttitle_tracknum");
+    if (loaded_track.text != "1.") {
+
+    }
+  }
+
+  play.click(); // autoplay
+ 
   player = document.getElementById("player");
   audio = document.getElementsByTagName('audio');
 }
